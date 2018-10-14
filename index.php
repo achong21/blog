aa40438437e1a408371e58aa74852f93265ac5a1
andy-prabowo <andyjawa51@gmail.com>
2018-10-15 00:25:41 +0700

    layout

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="description" content="Blog pertama ku" />
+	<meta name="author" content="Hafiz Prasetyo" />
+	<title>BLOG-GUE</title>
+	<!-- BOOTSTRAP CORE STYLE  -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet" />
+	<!-- FONT AWESOME STYLE  -->
+	<link href="assets/css/font-awesome.css" rel="stylesheet" />
+	<!-- DATATABLE STYLE  -->
+	<link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+	<!-- CUSTOM STYLE  -->
+	<link href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+	<div class="navbar navbar-inverse set-radius-zero">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="index.php" class="navbar-brand">
+					<img src="assets/img/logo.png"/>
+				</a>
+			</div>
+			<div class="right-div">
+				<a href="#" class="btn btn-danger pull-right">LOG OUT</a>
+			</div>
+		</div>
+	</div>
+	<section class="menu-section">
+		
+	</section>
+	<div class="wrapper"></div>
+	<section class="footer-section">
+		
+	</section>
+
+	<!-- CORE JQUERY  -->
+	<script src="assets/js/jquery-1.10.2.js"></script>
+	<!-- BOOTSTRAP SCRIPTS  -->
+	<script src="assets/js/bootstrap.js"></script>
+	<!-- DATATABLE SCRIPTS  -->
+	<script src="assets/js/dataTables/jquery.dataTables.js"></script>
+	<script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+	<!-- CUSTOM SCRIPTS  -->
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
